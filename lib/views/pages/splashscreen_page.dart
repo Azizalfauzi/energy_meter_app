3458fda39d70ff397df57d7b9c4da40859ec4641
Aziz Alfauzi <azizalfauzi@student.telkomuniversity.ac.id>
2023-03-24 16:38:58 +0700

    adding shared and creating splash screen

---
+++ lib/views/pages/splashscreen_page.dart
@@ -0,0 +1,43 @@
+import 'package:energy_meter_app/shared/shared.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreenPage extends StatefulWidget {
+  const SplashScreenPage({super.key});
+
+  @override
+  State<SplashScreenPage> createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff141519),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width / 2,
+            height: MediaQuery.of(context).size.height / 4,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/ic_primary.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              'Energy Meter',
+              style: whiteTextStyleInter.copyWith(
+                fontSize: 24,
+                fontWeight: bold,
+              ),
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
